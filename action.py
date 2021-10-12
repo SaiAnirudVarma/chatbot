c838934df1bcef6f647eb272b0cc402f7b24d66e
sai anirud varma uppalapati <saianirudhvarmauppalapati@gmail.com>
2021-10-12 18:56:24 -0400

    action file
    rasa core file patch 1

---
+++ action.py
@@ -0,0 +1,27 @@
+ffrom typing import Dict, Text, List, Optional, Any
+
+from rasa_sdk import Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.forms import FormValidationAction
+
+# show projects
+class ValidateRegisterForm(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_register_form"
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+    ) -> List[EventType]:
+        return []
+
+    def validate_name(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate slot value."""
+        if not slot_value:
+         return {"registeremail": None}
+        else:
+         return {"registeremail": slot_value}
